800d137b99a06dd020d432343e819ca596000701
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-01 17:36:36 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+Cliente c = new Cliente('g');
+System.out.println(c.calcularIdade());
+
+    }
+}
